dcf3f1a211f1186895a6415212c4b5db44f3010b
AbdullahAlazzani <43151817+AbdullahAlazzani@users.noreply.github.com>
2018-09-12 11:03:39 -0400

    Update TEST 1.py

--- TEST 1.py
+++ TEST 1.py
@@ -1,4 +1,3 @@
-
 """
 Abdullah Alazzani
 Plays a Game of Rock, Paper, and scissors
@@ -10,7 +9,8 @@ options = ["ROCK", "PAPER", "SCISSORS"]
 message = {
   "tie": "Yawn it's a tie!",
   "won": "Yay you won!",
-  "lost": "Aww you lost!"
+  "lost": "Aww you lost!",
+  "error": "That's not a valid game. Check your spelling!"
 }
 
 def decide_winner(user_choice, computer_choice):  # function implementation
@@ -20,16 +20,27 @@ def decide_winner(user_choice, computer_choice):  # function implementation
     print (message["tie"])
   elif user_choice == options[0] and computer_choice == options[2]:
       print (message["won"])
-  elif user_choice == options[1] and computer_choice == option[0]:
+  elif user_choice == options[1] and computer_choice == options[0]:
       print (message["won"])
-  elif user_choice == options[2] and computer_choice == option[1]:
+  elif user_choice == options[2] and computer_choice == options[1]:
     print (message["won"])
-  else:
+  elif user_choice == options[0] and computer_choice == options[1]:
+      print (message["lost"])
+  elif user_choice == options[1] and computer_choice == options[2]:
+      print (message["lost"])
+  elif user_choice == options[2] and computer_choice == options[0]:
     print (message["lost"])
+  else:
+    print (message["error"])
       
 def play_RPS():
-  user_choice = input("Enter Rock, Paper, or Scissor: ").upper()
-  computer_choice = options[randint(0,2)]
-  decide_winner(user_choice, computer_choice) # function call
-  
+  while True:
+    user_choice = input("Enter Rock, Paper, or Scissor: ").upper()
+
+    if user_choice == "STOP":
+      break
+    
+    computer_choice = options[randint(0,2)]
+    decide_winner(user_choice, computer_choice) # function call
+    print (" Print Stop if you want to Stop the Game!")
 play_RPS()
